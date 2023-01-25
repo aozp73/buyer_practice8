71482d559331478680380d00b9c99f5220de1436
aozp73 <aozp73@naver.com>
2023-01-25 20:08:46 +0900

    프로젝트 세팅 완료

---
+++ src/main/java/shop/mtcoding/buyer10/Buyer10Application.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.buyer10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buyer10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Buyer10Application.class, args);
+	}
+
+}
